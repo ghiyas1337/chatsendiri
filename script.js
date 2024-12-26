2a2bd9b7268d85886ba3e1b9e081f585f342c3dd
ghiyas1337 <ghiyasalok@gmail.com>
2024-12-26 15:36:25 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const photos = document.querySelectorAll('.photo img');
-    photos.forEach(photo => {
-        photo.addEventListener('click', function() {
-            alert('Anda mengklik gambar: ' + this.alt);
-        });
-    });
-});
